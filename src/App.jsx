2912ca3f195ee99d46bf4e3675eb0f118be3928d
LuizaMGama <luizamgama11@gmail.com>
2024-05-20 17:21:52 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import BuscarFilmes from "./componentes/BuscadeFilmes/BuscaFilmes";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <BuscarFilmes />
+    </div>
+  );
+}
